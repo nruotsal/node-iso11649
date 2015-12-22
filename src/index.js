403e21c07445eaa89c6ee5c23e15246e9173e22f
Niko Ruotsalainen <nruotsal@gmail.com>
2015-12-23 00:04:27 +0200

    Implement reference validate

--- src/index.js
+++ src/index.js
@@ -1,2 +1,38 @@
-export const validate = (reference) => reference === 'RF332348236'
-export const generate = () => 'RF332348236'
+const charTable = {
+  A: 10, B: 11, C: 12, D: 13, E: 14, F: 15, G: 16,
+  H: 17, I: 18, J: 19, K: 20, L: 21, M: 22, N: 23,
+  O: 24, P: 25, Q: 26, R: 27, S: 28, T: 29, U: 30,
+  V: 31, W: 32, X: 33, Y: 34, Z: 35
+}
+const divider = 97
+const format = /^RF[0-9]{2}[0-9A-Z]+$/
+
+const normalizeRef = (ref) =>
+  ref ? ref.replace(/ /g, '').toUpperCase() : ''
+
+const substituteChar = (char) =>
+  isNaN(char) ? charTable[char] : char
+
+const mod = (divident) => {
+  const chunks = divident.match(/.{1,7}/g)
+  return chunks.reduce((prev, curr) => `${prev}${curr}` % divider, '')
+}
+
+export const validate = (reference) => {
+  const normalizedRef = normalizeRef(reference)
+
+  const isValidFormat = (ref) =>
+    !!ref.match(format)
+
+  const isValidChecksum = (ref) => {
+    const preResult = moveRfToEnd(ref).map(substituteChar).join('')
+    return mod(preResult) === 1
+  }
+
+  const moveRfToEnd = (ref) =>
+    (ref.substr(4) + ref.substr(0, 4)).split('')
+
+  return normalizedRef.length <= 25
+    && isValidFormat(normalizedRef)
+    && isValidChecksum(normalizedRef)
+}
---
+++ test/validate.js
@@ -0,0 +1,42 @@
+import test from 'tape'
+import {validate} from '../src/index'
+
+test('validate should return true for valid reference with numbers', (assert) => {
+  const expected = true
+  const actual = validate('RF33 2348 236')
+
+  assert.equal(actual, expected)
+  assert.end()
+})
+
+test('validate should return true for valid reference with characters', (assert) => {
+  const expected = true
+  const actual = validate('RF19 AB2G 59X5 6V54 3')
+
+  assert.equal(actual, expected)
+  assert.end()
+})
+
+test('validate should return false for invalid reference', (assert) => {
+  const expected = false
+  const actual = validate('RF09 2348 236')
+
+  assert.equal(actual, expected)
+  assert.end()
+})
+
+test('validate should return true for valid 25 char long reference', (assert) => {
+  const expected = true
+  const actual = validate('RF40 1234 5678 9012 3456 7890 1')
+
+  assert.equal(actual, expected)
+  assert.end()
+})
+
+test('validate should return false for valid 26 char long reference', (assert) => {
+  const expected = false
+  const actual = validate('RF43 1234 5678 9012 3456 7890 12')
+
+  assert.equal(actual, expected)
+  assert.end()
+})
