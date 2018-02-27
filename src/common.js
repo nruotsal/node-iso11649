c7ff09ec6754b75f74d7156f8804182126f8082e
Niko Ruotsalainen <niko.ruotsalainen@newthings.co>
2018-02-27 10:40:46 +0200

    Refactor code in smaller files

---
+++ src/common.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+const charTable = {
+  A: 10, B: 11, C: 12, D: 13, E: 14, F: 15, G: 16,
+  H: 17, I: 18, J: 19, K: 20, L: 21, M: 22, N: 23,
+  O: 24, P: 25, Q: 26, R: 27, S: 28, T: 29, U: 30,
+  V: 31, W: 32, X: 33, Y: 34, Z: 35
+}
+/* eslint-enable */
+
+export const normalizeReference = reference =>
+  (reference ? (`${reference}`).replace(/ /g, '').toUpperCase() : '')
+
+export const substituteCharWithNumber = char =>
+  (Number.isNaN(Number(char)) ? charTable[char] : char)
+
+export const modulo97 = (divident) => {
+  const chunks = divident.match(/.{1,7}/g)
+  return chunks.reduce((prev, curr) => `${prev}${curr}` % 97, '')
+}
---
+++ src/validate.js
@@ -0,0 +1,21 @@
+import {normalizeReference, modulo97, substituteCharWithNumber} from './common'
+
+const REFERENCE_FORMAT = /^RF[0-9]{2}[0-9A-Z]+$/
+
+const moveRfToEnd = reference =>
+  (reference.substr(4) + reference.substr(0, 4)).split('')
+
+const isValidChecksum = (reference) => {
+  const preResult = moveRfToEnd(reference).map(substituteCharWithNumber).join('')
+  return modulo97(preResult) === 1
+}
+
+const isValidFormat = reference =>
+  !!reference.match(REFERENCE_FORMAT)
+
+export default (reference) => {
+  const normalizedRef = normalizeReference(reference)
+  return normalizedRef.length <= 25
+    && isValidFormat(normalizedRef)
+    && isValidChecksum(normalizedRef)
+}
