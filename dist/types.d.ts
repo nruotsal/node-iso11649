aaf03202e946248155b09687135e3ebe389e3599
David Knezić <davidknezic@gmail.com>
2020-03-30 17:15:54 +0200

    Add pretty print feature to generate function

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,4 +1,10 @@
-declare const generate: (reference?: string) => string;
-declare const validate: (reference: string) => boolean;
+export interface GenerateOptions {
+  reference?: string;
+  pretty?: boolean;
+}
+
+declare function generate(reference?: string) => string;
+declare function generate(options: GenerateOptions) => string;
+declare function validate(reference: string) => boolean;
 
 export { generate, validate }
